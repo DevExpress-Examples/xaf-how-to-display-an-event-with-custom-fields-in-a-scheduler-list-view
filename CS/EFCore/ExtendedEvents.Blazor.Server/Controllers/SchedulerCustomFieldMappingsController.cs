90d91e9a66943dc02ae81b6294a51ce59c827214
Anastasia Kiseleva <67680216+anastasiakiseleva@users.noreply.github.com>
2023-09-27 15:02:49 +0200

    Add solution

---
+++ CS/EFCore/ExtendedEvents.Blazor.Server/Controllers/SchedulerCustomFieldMappingsController.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.Blazor;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Scheduler.Blazor.Editors;
+using ExtendedEvents.Module.BusinessObjects;
+
+namespace ExtendedEvents.Blazor.Server.Controllers; 
+public class SchedulerCustomFieldMappingsController : ObjectViewController<ListView, ExtendedEvent> {
+    protected override void OnViewControlsCreated() {
+        base.OnViewControlsCreated();
+        var schedulerAdapter = ((SchedulerListEditor)View.Editor).GetSchedulerAdapter();
+        schedulerAdapter.SchedulerModel.DataStorage.AppointmentMappings.CustomFieldMappings = new[] {
+            new DxSchedulerCustomFieldMapping {
+                Name = "SimpleField",
+                Mapping = nameof(ExtendedEvent.CustomSimpleTypeField)
+            },
+            new DxSchedulerCustomFieldMapping {
+                Name = "ReferenceField",
+                Mapping = $"{nameof(ExtendedEvent.CustomReferenceTypeField)}.{nameof(CustomReferenceTypeField.Name)}"
+            }
+        };
+
+        schedulerAdapter.DayViewModel.VerticalAppointmentTemplate = CustomAppointmentTemplate.Create();
+        schedulerAdapter.DayViewModel.HorizontalAppointmentTemplate = CustomAppointmentTemplate.Create();
+    }
+}
